5948dc732f356287e3bc6af8d2ea59ae5ef02edd
nikitaandreev96 <n.andreev@fairyhosting.com>
2020-11-02 14:40:36 +0200

    empty

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php get_header(); ?>
 <main>
-    <h1>Tallinna Pae Gümnaasium</h1>
-    <h2>Честность | Целеустремлённость | Трудолюбие</h2>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-6">
-                <div class="info-1">
-                    <h2>Koolitöö muudatused ja õpetajate asendused</h2>
-                </div>
-            </div>
-            <div class="col-6">
-                <div class="info-2">
-                    <h2>Uudised</h2>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div id="particles-js"></div>
+    
 </main>
 <?php get_footer(); ?>
