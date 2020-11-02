36138e1886f308c6484f86e865e83b38396d320d
Magalmund <poker6-96@mail.ru>
2020-11-02 21:18:36 +0200

    step 1

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
 <?php get_header(); ?>
 <main>
-    <h1>Tallinna Pae Gümnaasium</h1>
-    <h2>Честность | Целеустремлённость | Трудолюбие</h2>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-6">
-                <div class="info-1">
-                    <h2>Koolitöö muudatused ja õpetajate asendused</h2>
-                </div>
-            </div>
-            <div class="col-6">
-                <div class="info-2">
-                    <h2>Uudised</h2>
-                </div>
+    <div id="banner" class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-10">
+                <h1>Tallinna Pae Gumnaasium</h1>
             </div>
         </div>
     </div>
-    <div id="particles-js"></div>
 </main>
 <?php get_footer(); ?>
