178f7ad5e7897d3de315c9396283280d728519a9
nikitaandreev96 <n.andreev@fairyhosting.com>
2020-11-18 16:01:02 +0200

    change

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
+window.onload = function() {
+    const navbar = document.getElementById('navbar');
+    window.addEventListener('scroll', scroller);
+    
+    function scroller() {
+        var position = window.pageYOffset;
+
+        if (position > 96) {
+            navbar.classList.add('scrolldown');
+        } else {
+            navbar.classList.remove('scrolldown');
+        }
+    }
+}
+
+
+
+
+
 var swiper = new Swiper('.swiper-container', {
         slidesPerView: 1,
         spaceBetween: 0,
         loop: true,
         /*autoplay: {
-            delay: 7500,
+            delay: 5000,
             disableOnInteraction: false,
         }*/
     });
