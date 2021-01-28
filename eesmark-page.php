abe1c03d9dc4d449d02226a7b6c2024de6cbb4fb
Magalmund <poker6-96@mail.ru>
2021-01-28 20:52:55 +0200

    change

---
+++ eesmark-page.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Template Name: eesmark-page
+*/
+get_header(); ?>
+    <div id=page-banner class="container-fluid">
+        <div class="row">
+            <div class="col-lg-1"></div>
+            <div class="col-lg-8">
+                <h2><?php the_title(); ?></h2>
+            </div>
+            <div class="col-lg-3"></div>
+        </div>
+    </div>
+    
+    <div id="eesmark-content" class="container-fluid page-content common-page">
+        <div class="row">
+            <div class="col-lg-5">
+                <div class="row">
+                        <?php $query = new WP_Query( array( 'category_name' => 'eesmark-category' ) );?>	
+                            <?php 
+                            $idx = 1;
+                            while ($query -> have_posts()) : $query -> the_post(); ?>
+                            <? if( $idx % 2 == 0) { ?>
+                                <div class="col-lg-10">
+                                    <div class="table">
+                                        <?php the_content(); ?>
+                                    </div>
+                                </div>
+                            <?php }?>
+                        <?php 
+                        $idx++;
+                        endwhile; 
+                        ?>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <div class="row">
+                    <?php $query = new WP_Query( array( 'category_name' => 'eesmark-category' ) );?>	
+                            <?php 
+                            $idx = 1;
+                            while ($query -> have_posts()) : $query -> the_post(); ?>
+                            <? if( $idx % 2 == 1) { ?>
+                                <div class="col-lg-10">
+                                    <div class="table">
+                                        <?php the_content(); ?>
+                                    </div>
+                                </div>
+                            <?php }?>
+                        <?php 
+                        $idx++;
+                        endwhile; 
+                        ?>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
