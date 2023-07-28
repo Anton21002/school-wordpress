463de7188606762922f767907cbf929755761dbe
Anton21002 <anton.satalin@student.hamk.fi>
2023-07-28 13:26:26 +0300

    Juhtkond page listing update
    Added a new way to display juhtkond (images and stylisation WIP)

---
+++ Infohandler.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS infohandler 
+(
+`ItemID` INT(10) AUTO_INCREMENT UNIQUE,
+`ID` INT(10),
+`Datatype` VARCHAR(80),
+`ItemDescription` VARCHAR(300),
+`RelAtr1` VARCHAR(1000),
+`RelAtr2` VARCHAR(1000),
+`RelAtr3` VARCHAR(1000),
+`RelAtr4` VARCHAR(1000),
+`RelAtr5` VARCHAR(1000),
+`RelAtr6` VARCHAR(1000),
+`RelAtr7` VARCHAR(1000),
+`RelAtr8` VARCHAR(1000),
+`RelAtr9` VARCHAR(1000),
+`RelAtr0` VARCHAR(1000)
+) ENGINE=InnoDB AUTO_INCREMENT=3330 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+INSERT INTO infohandler 
+(Datatype,ItemDescription,RelAtr1,RelAtr2,RelAtr3,RelAtr4) VALUES 
+('LIST',"direktori kontaktandmed","Изабелла Рийтсаар","Директор","621 1611","direktor@pae.tln.edu.ee"),
+('LIST',"direktori kontaktandmed","Светлана Каст","HEV координатор","621 1984","s.borodavko@pae.tln.edu.ee"),
+('LIST',"direktori kontaktandmed","Наталия Преснецова","Заведующая учебной частью программы погружения","623 1223","n.voronova@pae.tln.edu.ee"),
+('LIST',"direktori kontaktandmed","Павел Алонов","Руководитель по проектам, учитель","623 1223","p.alonov@pae.tln.edu.ee"),
+('LIST',"direktori kontaktandmed","Оксана Шеленёва","Заведующая учебной частью ","6623 1223 ","o.selenjova@pae.tln.edu.ee"),
+('LIST',"direktori kontaktandmed","Валентина Диденко","Заведующая хозяйственной частью","621 1963","majajuht@pae.tln.edu.ee"),
+('LIST',"direktori kontaktandmed","Леа Лойде ","Заведующая канцелярией","621 0230","pae@pae.tln.edu.ee"),
+('LIST',"direktori kontaktandmed","Анна Луйк","Руководитель по интересам","621 1963","a.hurm@pae.tln.edu.ee")
+;
--- juhtkond-page.php
+++ juhtkond-page.php
@@ -19,15 +19,58 @@ get_header(); ?>
                         <?php $query = new WP_Query( array( 'category_name' => 'juhtkond-category' ) );?>	
                             <?php 
                             while ($query -> have_posts()) : $query -> the_post(); ?>
-                                <div class="col-lg-12 col-xl-6" data-aos="flip-left" data-aos-easing="ease-out-cubic">
+                                <div class="col-lg-12 col-xl-6 juhtkondtables" data-aos="flip-left" data-aos-easing="ease-out-cubic">
                                     <h3><?php the_title();?></h3>
                                     <p><?php the_content(); ?></p>
                                 </div>
                         <?php 
-                        endwhile; 
-                        ?>
+                        endwhile; ?>
+                        
+                        
+                        
                 </div>
             </div>
-        </div>
-    </div>
+        </div> <!-- <figure> -> (<img> <figcaption>-> 4<p> )--> 
+         <div class="juhtkondcards">   
+        <?php 
+            global $wpdb;
+            $result = $wpdb->get_results( "SELECT * FROM infohandler");
+            foreach ( $result as $print ) { 
+            $relatr1=$print->RelAtr1;   
+            $relatr2=$print->RelAtr2;
+            $relatr3=$print->RelAtr3; 
+            $relatr4=$print->RelAtr4; ?>
+            <figure class="juhtcard"><img src="" alt="" class="juhtimg">
+                <figcaption>
+                    <p><?php  echo $relatr1 ."</p>
+                    <p>". $relatr2 . "</p>
+                    <p> tel : ". $relatr3 . "</p>
+                    <p> email : ". $relatr4 . "</p>
+                </figcaption>
+            </figure>";  
+                    } ?>
+    </div></div>
 <?php get_footer(); ?>
+
+<?php /* table example
+    <table class="juhtkondtable"><tr>    
+    <th>Имя, фамилия</th>
+    <th>Должность</th>
+    <th>Номер телефона</th>
+    <th>Электронная почта</th></tr>
+    <?php 
+            global $wpdb;
+            $result = $wpdb->get_results( "SELECT * FROM infohandler");
+            foreach ( $result as $print ) { 
+            $relatr1=$print->RelAtr1;   
+            $relatr2=$print->RelAtr2;
+            $relatr3=$print->RelAtr3; 
+            $relatr4=$print->RelAtr4; ?>
+            <tr class="juhkkondrow">
+                <td><?php  echo $relatr1 ."</td><td>".
+                  $relatr2 . "</td><td>".
+                  $relatr3 . "</td><td>".
+                  $relatr4 . "</td>
+            </tr>";  } ?>
+    </table>    
+*/?>
